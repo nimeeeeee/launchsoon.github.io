41d6bed27e1e212a7678d70a1371d1e8c87eec4a
nimeeeeee <163139487+nimeeeeee@users.noreply.github.com>
2024-03-12 21:13:38 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,25 @@
+var countDownDate = new Date("April 1, 2024 00:00:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").textContent = "00";
+        document.getElementById("hours").textContent = "00";
+        document.getElementById("minutes").textContent = "00";
+        document.getElementById("seconds").textContent = "00";
+    }
+
+},1000)
